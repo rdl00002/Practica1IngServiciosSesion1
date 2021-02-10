b701523754eb8616cca20a2acd6d6f2ff0ebd910
Rubén Domenech López <rubencillodl@hotmail.com>
2021-02-10 10:45:55 +0100

    casi hecha

--- Practica1IngServicios/src/Package/Servlet.java
+++ Practica1IngServicios/src/Package/Servlet.java
@@ -1,6 +1,10 @@
 package Package;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Servlet
  */
-@WebServlet("//localhost:8081/Practica1IngServicios/Servlet")
+@WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -20,7 +24,6 @@ public class Servlet extends HttpServlet {
     public Servlet() {
         super();
         // TODO Auto-generated constructor stub
-        
     }
 
 	/**
@@ -29,6 +32,29 @@ public class Servlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		System.out.println("--> getHeaderNames: ");
+		Map<String, String> map = new HashMap<String, String>();
+
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+            System.out.println(map.toString());
+        }
+        
+        System.out.println("--> getHeaders: ");
+		Map<String, String> mapheader = new HashMap<String, String>();
+
+        Enumeration headers = request.getHeaders(getServletInfo());
+        while (headers.hasMoreElements()) {
+            String key = (String) headers.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+            System.out.println(mapheader.toString());
+        }
+		
 	}
 
 	/**
